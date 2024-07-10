a265f3dad012252fd53bdc1cf5f8c148142a8937
Shashank <vshashank00@gmail.com>
2024-07-10 09:20:53 +0530

    added test runner

--- src/test/java/org/example/TestRunner.java
+++ src/test/java/org/example/TestRunner.java
@@ -1,15 +1,27 @@
 package org.example;
 
 import org.testng.TestNG;
+import org.testng.annotations.Test;
 
 import java.util.ArrayList;
 
 public class TestRunner {
-    public static void main(String[] args) {
-        TestNG testNG=new TestNG();
+    TestNG testNG;
+    @Test
+    void parallel_in_oneBrowser(){
+       testNG =new TestNG();
         ArrayList<String> arrayList=new ArrayList<>();
         arrayList.add(System.getProperty("user.dir")+"/testng.xml");
         testNG.setTestSuites(arrayList);
         testNG.run();
+    }
+    @Test
+    void parallel_in_twoDifferentBrowser(){
+        testNG=new TestNG();
+        ArrayList<String>arrayList=new ArrayList<>();
+        arrayList.add("parallel.xml");
+        testNG.setTestSuites(arrayList);
+        testNG.run();
+
     }
 }
